30c34b0d484d9f99f5e6ca3ca5a395c18c4da965
Mark Maurer <mark@technomancer.org>
2024-05-15 23:16:49 -0500

    feat: added initialization routine to create environment

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -1,34 +1,96 @@
 package main
 
 import (
-    "fmt"
-    "github.com/davecgh/go-spew/spew"
-
-    cfgparse "pmancli/internal/CfgParse"
+	// "fmt"
+    "os"
+    "log"
+    "database/sql"
+	// "github.com/davecgh/go-spew/spew"
+    //sqlite "github.com/mattn/go-sqlite3"
+	// cfgparse "pmancli/internal/CfgParse"
 )
 
+var configDirBase string = ".pmancli"
+
+/* ---
+ * Intialize the environtment:
+ *  ~/.pmancli
+ *      bookmarks
+ *          <collectionname>.json
+ *      environments
+ *          <collectionname>.json
+ *      results
+ *          queryresults.db
+ */
+func InitializeEnvironment() {
+    var configDir string = os.Getenv("HOME") + "/" + configDirBase
+
+    _, err := os.Stat(configDir)
+    if os.IsNotExist(err) {
+        err = os.Mkdir(configDir, 0750)
+        if err != nil && !os.IsExist(err) {
+            log.Fatal(err)
+        }
+    }
+
+    var envdirs = [3]string{configDir+"/bookmarks", configDir+"/environments", configDir+"/results"}
+    for _, edir := range envdirs {
+        _, err := os.Stat(edir)
+        if os.IsNotExist(err) {
+            err = os.Mkdir(edir, 0750)
+            if err != nil && !os.IsExist(err) {
+                log.Fatal(err)
+            }
+        }
+    }
+
+    var resultsDb = configDir + "/results/results.db"
+    _, err = os.Stat(resultsDb)
+    if os.IsNotExist(err) {
+        f, ferr := os.Create(resultsDb)
+        if ferr != nil {
+            log.Fatal(ferr.Error())
+        }
+        f.Close()
+    }
+
+    //sqlfh, _ := sql.Open("sqlite3", resultsDb)
+    //defer sqlfh.Close()
+
+    /*
+        db, sqlerr := sqlite.Open(resultsDb, ":memory")
+        if sqlerr != nil {
+            log.Fatal("Failed to create sqlite3 results db: ", sqlerr)
+        }    
+    */
+}
 
 func main() {
-    var bmark []cfgparse.Bookmark
-    var en []cfgparse.Environment
+	// var bmark []cfgparse.Bookmark
+	// var en []cfgparse.Environment
 
-    bmark = cfgparse.ParseBookmarks("./bookmark-defs.json")
-    en = cfgparse.ParseEnvironments("./env-defs.json")
+    InitializeEnvironment()
 
-    fmt.Printf("Dumping bookmarks...\n")
-    spew.Dump(bmark)
-    fmt.Printf("\nDumping Environments...\n")
-    spew.Dump(en)
 
-    var newbmark cfgparse.Bookmark
+    /*
+	bmark = cfgparse.ParseBookmarks("./bookmark-defs.json")
+	en = cfgparse.ParseEnvironments("./env-defs.json")
 
-    newbmark.Id = "2"
-    newbmark.Name = "Second Bookmark"
-    newbmark.Request = "https://www.konectauto.com/"
-    newbmark.Method = "GET"
-    newbmark.Body.Mode = ""
+	fmt.Printf("Dumping bookmarks...\n")
+	spew.Dump(bmark)
+	fmt.Printf("\nDumping Environments...\n")
+	spew.Dump(en)
 
-    cfgparse.RunBookmark(bmark[0])
+	var newbmark cfgparse.Bookmark
 
-    cfgparse.AddBookmark(newbmark, bmark, "./bookmark-defs.json")
+	newbmark.Id = "2"
+	newbmark.Name = "Second Bookmark"
+	newbmark.Request = "https://www.konectauto.com/"
+	newbmark.Method = "GET"
+	newbmark.Body.Mode = ""
+
+	cfgparse.RunBookmark(bmark[0])
+
+	cfgparse.AddBookmark(newbmark, bmark, "./bookmark-defs.json")
+    */
 }
